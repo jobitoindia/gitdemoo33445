1f62e1130b2e66737c448aadf0b8a959d1c126c1
jobitoindia <lahupande2017@gmail.com>
2024-05-06 11:54:30 +0530

    hii

--- src/demo123/demo123455.java
+++ src/demo123/demo123455.java
@@ -1,5 +1,14 @@
 package demo123;
 
-public class demo123455 {
-
-}
+public class demo123455 
+{   
+	
+	
+	
+	private void sysout() { "hiiii"
+	// TODO Auto-generated method stub
+sysout();
+} 
+sysout
+} 
+sysout
